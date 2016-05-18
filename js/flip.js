7c126cda1c78509f5ef6d013b54f3a8d7934c79e
ariel0058 <ariel0058@hotmail.com>
2016-05-18 15:26:30 -0700

    Yao updates the hpBar and part of score

--- js/flip.js
+++ js/flip.js
@@ -1,13 +1,25 @@
-			/*
-			   when wind is loaded flip puzzles after 3 seconds
-			*/
-			window.onload = function(){
-				setTimeout(function(){
-					var flippers = document.getElementsByClassName("flipper");
-					//confirm(flippers.length);
-					for(var i=0; i<flippers.length; i++){
-						flippers[i].style.transform = "rotateY(180deg)";
-					}					
-				}
-				, 500);			
-			}
+/*
+	when wind is loaded flip puzzles after 3 seconds
+*/
+var importJS=document.createElement("script");
+    //import hp.js
+    importJS.setAttribute("type","text/javascript");
+    importJS.setAttribute("src","./js/hp.js");
+    document.body.appendChild(importJS);
+
+window.onload = function(){
+	setTimeout(function(){
+        var flippers = document.getElementsByClassName("flipper");
+        //confirm(flippers.length);
+        for(var i=0; i<flippers.length; i++){
+            flippers[i].style.transform = "rotateY(180deg)";
+        }					
+    }
+	, 2000);	
+    
+    hpContainer = document.getElementById('hp');
+	init();
+	for(var i = 0; i < hp; i++)
+		hpContainer.appendChild(heart[i]);
+    timer = setInterval('run()', 1000);
+}
