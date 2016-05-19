42558a7b2bc3e9189a47d1078d60ccaceca01a11
Rina Hong <Rina Hong>
2016-05-18 23:52:20 -0700

    pass and fail page bootstrap

---
+++ js/passGame.js
@@ -0,0 +1,13 @@
+
+var numGarbages = 0;
+var flippers = document.getElementsByClassName("flipper");
+for(var i=0; i<flippers.length; i++) {
+	numGarbages = numGarbages + 1;
+}
+
+while() {
+	numGarbages = numGarbages - 1;
+	if(numGarbages == 0) {
+		window.location.href='pass.html';
+	} 
+}
