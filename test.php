0f22b57b4b99211eb584011716231c8e8abf95c2
Rebecca <544846169@qq.com>
2016-05-19 00:14:50 -0700

    rebe add new php files

---
+++ test.php
@@ -0,0 +1,22 @@
+<html>
+	<head><meta charset="utf-8"></head>
+	<body>
+		<form action="leader.php" method="post">
+
+			<table>
+				<tr>
+					<td>Enter your name</td>
+					<td><input name="username" /></td>
+				</tr>
+				<tr>
+					<td>Enter your score</td>
+					<td><input name="mark" /></td>
+				</tr>
+				<tr>
+					<td><input type="submit" /></td>
+				</tr>
+			</table>
+		
+		</form>
+	</body>
+</html>
