9ea6ea104b7db8c04da22c8460707f076d76f267
Rebecca <544846169@qq.com>
2016-05-20 16:45:38 -0700

    rebeb chang

--- js/memorizetime.js
+++ js/memorizetime.js
@@ -6,24 +6,24 @@ var s = 10;
 */
 function showtime(){
 	document.getElementById('m').innerHTML = m;
-	document.t('s').inerHTML = s;
-	s = s-1;
-	if (m!=0) {
-		if(s == 0) {
-			m--;
-			s=60;
+	document.getElementById('s').innerHTML = s;
+		s = s-1;
+		if (m!=0) {
+			if(s == 0) {
+				m--;
+				s=60;
+			}
+		} else if (m = 0 && s !=0) {
+			s--;
+		}else if(m==0 && s==0){
+			var time = document.getElementById("timer");
+			time.style.visibility = 'hidden';
 		}
-	} else if (m = 0 && s !=0) {
-		s--;
-	}else if(m==0 && s==0){
-		var time = document.getElementById("timer");
-		time.style.visibility = 'hidden';
-	}
 }
 /*
     set the interval for the counting down time display
 */
 clearInterval(settime); 
-    var settime = setInterval(function(){
-        showtime();
-    },1000);
+var settime = setInterval(function(){
+	showtime();
+},1000);
