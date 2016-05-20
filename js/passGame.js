84d0ea720c0233b28d0bb577b90ef1afc9771494
Rina Hong <Rina Hong>
2016-05-19 17:21:20 -0700

    some unused comment deleted

--- js/passGame.js
+++ js/passGame.js
@@ -1,19 +1,3 @@
-/*
-var numGarbagesRest;
-var  flippers= document.getElementsByClassName("flipper");
-numGarbagesRest = flippers.length;
-
-function dropGarbage(){
-	numGarbagesRest--;
-}
-
-function checkSuccess(){
-	return numGarbagesRest == 0; 
-}
-*/
-
-
-
 
 var garbageNotDraggedYet = 0;
 var playedEasterGame = 0;
@@ -27,6 +11,3 @@ function dropGarbage(){
 function checkSuccess(){
 	return garbageNotDraggedYet == 0; 
 }
-
-/*----------------NOT DONE YET! ---------------*/
-/*----------------FUNCTION FOR NAVIGATING TO PASS PAGE IS IN macth.js---------------*/
