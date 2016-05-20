3793e3f14f0a265e1c5ce99abfadf86a86126ae3
Rina Hong <Rina Hong>
2016-05-19 17:20:14 -0700

    some changes deleted

--- js/match.js
+++ js/match.js
@@ -23,20 +23,6 @@ function match(){
 
 }
 
-/*------ Below the script is for navigating to pass page when a player finishes the game successfully------------*/
-
-/*var garbageNotDraggedYet = 0;
-var playedEasterGame = 0;
-var numDraggable= document.getElementsByClassName("draggableTrash");
-garbageNotDraggedYet = numDraggable.length;
-
-function dropGarbage(){
-	garbageNotDraggedYet--;
-}
-
-function checkSuccess(){
-	return garbageNotDraggedYet == 0; 
-}*/
 
 
 
