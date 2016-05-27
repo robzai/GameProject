79b2b9bedf9dfcb10bbaa67d21100d0e51f1318b
Leo <robzai@hotmial.com>
2016-05-26 19:08:23 -0700

    medal

---
+++ js/medal.js
@@ -0,0 +1,26 @@
+/*
+    import totalScore.js
+*/
+var importTotalScore=document.createElement("script");
+    importTotalScore.setAttribute("type","text/javascript");
+    importTotalScore.setAttribute("src","./js/totalScore.js");
+    document.body.appendChild(importTotalScore);
+
+localStorage.medal1 = 0;
+localStorage.medal2 = 0;
+localStorage.medal3 = 0;
+	
+function showMedalAchieve(){
+	if(getTotal()>500 && localStorage.medal1 == 0){
+		alert("medal1 achieved");
+		localStorage.medal1 = 1;
+	}
+	if(getTotal()>2000 && localStorage.medal1 == 0){
+		alert("medal2 achieved");
+		localStorage.medal2 = 1;
+	}
+	if(getTotal()>6000 && localStorage.medal1 == 0){
+		alert("medal3 achieved");
+		localStorage.medal3 = 1;
+	}
+}
