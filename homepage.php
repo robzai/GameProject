74a49c629c37c9a322c1bb9b8c7ba2d2815f7ddb
Rebecca <544846169@qq.com>
2016-05-26 17:03:58 -0700

    rebe ooo

--- homepage.php
+++ homepage.php
@@ -1,15 +1,25 @@
-<?php
+<?php 
 	session_start();
-	if($_POST['pwd'] != $_POST['repwd']) {
-		echo "your passwords don't match";		
+	$db = mysqli_connect("localhost", "root","root","score") or die(mysqli_connect_error());
+	$sql = "SELECT pwd FROM rank WHERE userName LIKE '$_POST[username]'";
+	$result = mysqli_query($db,$sql);
+	$num = mysqli_num_rows($result);
+	if ($num <= 0) {
+		echo "You need to sign up first!";		
 		echo "<script>setTimeout('window.location=\'signup.php\';', 3000)</script>";
 		die();
-	} else {
-		$db = mysqli_connect("localhost", "root","root","score") or die(mysqli_connect_error());
-		$sql = "INSERT INTO rank VALUES ('$_POST[username]','$_POST[pwd]',0,0,0,0,0)";
-		mysqli_query($db,$sql);
-		$_SESSION['username']=$_POST['username'];
-		$_SESSION['pwd']=$_POST['pwd'];		
+	}
+	while($row = mysqli_fetch_assoc($result)){
+		foreach($row as $key=>$value){
+			if (strcmp($value,$_POST['pwd']) == 0) {
+				$_SESSION['username']=$_POST['username'];
+				$_SESSION['pwd']=$_POST['pwd'];
+			} else {
+				echo "please check your username and password again!";
+				echo "<script>setTimeout('window.location=\'login.php\';', 3000)</script>";
+				die();
+			}	
+		}
 	}
 ?>
 <html lang="en">
@@ -36,12 +46,14 @@
 							<a href = "level1.php"> <img src = "pic/button/start.png"> </a>
 						</div>
 						<div class = "tutorial">
-							<a href = "signup.php"> <img src = "pic/button/tutorial.png"> </a>
+							<a href = "#"> <img src = "pic/button/tutorial.png"> </a>
 						</div>
 						<div class = "archi">
-							<a href = "#"> <img src = "pic/button/archi.png"> </a>
-						</div>		
-						<a href="logout.php">logout</a>
+							<a href = "reward.php"> <img src = "pic/button/archi.png"> </a>
+						</div>	
+						<div class = "logout">
+							<a href="logout.php">logout</a>	
+						</div>
 					</div>	
 				</div>
 				<div class="col-xs-4 visible-md visible-lg"></div>
--- reward.php
+++ reward.php
@@ -4,10 +4,10 @@
 	$sql = "SELECT * FROM rank WHERE userName LIKE '$_SESSION[username]'";
 	$result = mysqli_query($db,$sql);
 	$row = mysqli_fetch_assoc($result);
-	$one = $row["1sttool"];	
-	$two = $row["2ndtool"];	
-	$three = $row["3rdtool"];	
-
+	$total = $row["score"];
+	$cu = $row["1sttool"];	
+	$silver = $row["2ndtool"];	
+	$golden = $row["3rdtool"];	
 ?>
 
 <html lang="en">
@@ -23,44 +23,45 @@
 				<div class="col-xs-4 visible-md visible-lg"></div>
 				<div class="col-xs-4">
 					<div class = "main">
+						<div class = "displayscore">
+							<?php echo "Highest Score: ".$total ?>
+						</div>
 						<div class = "first">
 							<?php 
-								if ( $one == 0 ) {
-									echo "<div id = '1sttool'>";
-									echo "<form action=' $_SERVER['PHP_SELF']' method='get'>";
-									echo "<img src='pic/button/canfetitem.png' alt='item button' name = 'add'>";
-									echo "</form>";
-									echo "</div>";
-								} else {
+								if ( $total > 500) {
 									echo "<div id = '1sttool'>";
 									echo "<img src='pic/button/getitem.png' alt='item button'>";
+									echo "</div>";									
+								} else {
+									echo "<div id = '1sttool'>";
+									echo "<img src='pic/button/canfetitem.png' alt='item button'>";
 									echo "</div>";								
 								}
 							?>
 						</div>
 						<div class = "second">
 							<?php 
-								if ( $two == 0 ) {
+								if ( $total > 2000 ) {
+									echo "<div id = '1sttool'>";
+									echo "<img src='pic/button/getitem.png' alt='item button'>";								
+									echo "</div>";
+								} else {
 									echo "<div id = '1sttool'>";
 									echo "<img src='pic/button/canfetitem.png' alt='item button' onclick = ''>";
-									echo "</div>";
-								} else {
-									echo "<div id = '1sttool'>";
-									echo "	<img src='pic/button/getitem.png' alt='item button'>";
 									echo "</div>";								
 								}
 							?>
 						</div>
 						<div class = "second">
 							<?php 
-								if ( $three == 0 ) {
-									echo "<div id = '1sttool'>";
-									echo "	<img src='pic/button/canfetitem.png' alt='item button' onclick = ''>";
-									echo "</div>";
-								} else {
+								if ( $total > 4000 ) {
 									echo "<div id = '1sttool'>";
 									echo "	<img src='pic/button/getitem.png' alt='item button'>";
-									echo "</div>";								
+									echo "</div>";	
+								} else {
+									echo "<div id = '1sttool'>";
+									echo "	<img src='pic/button/canfetitem.png' alt='item button' onclick = ''>";
+									echo "</div>";							
 								}
 							?>	
 						</div>
